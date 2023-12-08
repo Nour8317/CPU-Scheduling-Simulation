c4eaf004a0a484c18381e8887e7c34f40841ebe5
Nour El-deen <101468589+Nour8317@users.noreply.github.com>
2023-12-09 01:37:41 +0200

    Add files via upload
    Initial Files

---
+++ CPU_Scheduling/src/main/java/cpu/cpu_scheduling/CPU_Scheduling.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cpu.cpu_scheduling;
+
+/**
+ *
+ * @author Nour eldeen
+ */
+public class CPU_Scheduling {
+
+    public static void main(String[] args) {
+        main.startGUI();
+    }
+}
